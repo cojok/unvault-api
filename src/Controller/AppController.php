945d247826c7ed5a68016138f746a96a2b57e007
Flaviu Cojocariu <cojokka@gmail.com>
2016-05-15 17:25:45 +0200

    CORS fix

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -59,6 +59,12 @@ class AppController extends Controller
     */
 	public function beforeFilter(Event $event)
 	{
+		$this->response->header('Access-Control-Allow-Origin','*');
+		$this->response->header('Access-Control-Allow-Methods','*');
+		$this->response->header('Access-Control-Allow-Headers','X-Requested-With');
+		$this->response->header('Access-Control-Allow-Headers','Content-Type, x-xsrf-token');
+		$this->response->header('Access-Control-Max-Age','172800');
+		
 		$this->user_id = $this->Auth->user('id');
 
 		// validate user token for logged user
